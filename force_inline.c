61185de0665db234ee06e918fcdefab67d856209
Elvis Teixeira <elvismtt@gmail.com>
2017-01-13 19:02:57 -0300

    Create force_inline.c

---
+++ force_inline.c
@@ -0,0 +1,6 @@
+/* the best one can do to try to make a function inline
+#if defined(_MSC_VER)
+#  define FORCE_INLINE    __forceinline
+#else
+#  define FORCE_INLINE inline __attribute__((always_inline))
+#endif
